7ab3fbfb6e7268726af0c68dd84c389409fe4faa
lehuutai0310 <lehuutai0310@gmail.com>
2023-10-20 00:16:36 +0700

    test deploy vercel

--- practice-one/src/App.tsx
+++ practice-one/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route element={<MainLayout />}>
           {routers.map((item) => (
             <Route key={item.path} path={item.path} element={item.element} />
