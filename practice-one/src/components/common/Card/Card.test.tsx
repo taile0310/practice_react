8cc37145371e188213aaf1c59bd6c760368a4698
lehuutai0310 <lehuutai0310@gmail.com>
2023-11-13 14:12:37 +0700

    Feature: test component card

---
+++ practice-one/src/components/common/Card/Card.test.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Card from ".";
+import { VARIANT } from "../../../types/Variant";
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+describe("Card Component", () => {
+  const props = {
+    titleCard: "Your Subtotal",
+    variants: VARIANT.PRIMARY,
+  };
+  it("Render correctly with default props", () => {
+    const container = render(<Card {...props} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Render with variant is secondary", () => {
+    const container = render(<Card {...props} variants={VARIANT.SECONDARY} />);
+    expect(container).toMatchSnapshot();
+  });
+});
--- practice-one/src/helpers/CalculatorToltalPrice.ts
+++ practice-one/src/helpers/CalculatorToltalPrice.ts
@@ -3,6 +3,8 @@ import { CustomProductProps } from "../types/Product";
 
 // Calculate total value
 export const calculatorTotalPrice = (carts: CustomProductProps[]): number => {
+  if (!carts) return 0;
+
   const totalPrice = carts.reduce(
     (accumulator: number, item: CustomProductProps) => {
       const itemTotal = (item.quantity || 0) * (item.price || 0);
