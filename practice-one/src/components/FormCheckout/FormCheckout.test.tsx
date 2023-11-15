81fd78a52f9af3aef9078d1effebb0c28485215e
lehuutai0310 <lehuutai0310@gmail.com>
2023-11-15 14:39:45 +0700

    Update test case for components

--- practice-one/src/components/FormCheckout/FormCheckout.test.tsx
+++ practice-one/src/components/FormCheckout/FormCheckout.test.tsx
@@ -1,14 +1,14 @@
 import { render } from "@testing-library/react";
 import FormCheckout from ".";
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: jest.fn(),
-}));
+import { BrowserRouter } from "react-router-dom";
 
 describe("FormCheckout Component", () => {
   it("Render component correctly", () => {
-    const container = render(<FormCheckout />);
+    const container = render(
+      <BrowserRouter>
+        <FormCheckout />
+      </BrowserRouter>
+    );
     expect(container).toMatchSnapshot();
   });
 });
